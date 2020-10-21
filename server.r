0e0146101a915f999025790eb8798d5567a37056
RJNunez <roa310@g.harvard.edu>
2020-10-20 22:44:48 -0400

    adding new functionalities to the shiny app

--- server.r
+++ server.r
@@ -1,31 +1,63 @@
 source("init.R")
-my_palette <- c("#f0f0f0", "#cb181d", "#2171b5", "#6a51a3", "#238b45")
+load("rda/covid19-usa.rda")
+my_palette <- c("#f0f0f0", "#cb181d", "#2171b5", "gold", "#238b45", "#6a51a3")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
   output$percent_change <- renderPlot({
     
-    percent_change %>%
+    # -- State specific data
+    states_dat <- percent_change %>%
       filter(type == "weighted") %>%
       filter(state %in% input$state) %>%
       filter(date >= input$range[1], date <= input$range[2]) %>%
-      mutate(state = factor(state, levels = input$state)) %>%
-      ggplot(aes(date, fitted, color=state, fill=state)) +
+      mutate(state = factor(state, levels = input$state))
+    
+    # -- Used for labeling
+    last_dp <- states_dat %>%
+      group_by(state) %>%
+      filter(date >= max(date)-8)
+    
+    # -- Used to determine y-axis
+    y_limits <- range(states_dat$fitted)
+    
+
+    states_dat %>%
+      ggplot(aes(date, fitted, label=state, color=state)) +
       geom_hline(yintercept = 0, color="#525252", lty=2) +
-      geom_point(aes(date, observed/expected - 1), alpha=0.50, show.legend = FALSE) +
-      geom_ribbon(aes(ymin=lwr, ymax=upr), alpha=0.20, color=NA, show.legend = FALSE) +
-      geom_line() +
+      geom_line(aes(date, fitted, group=state), color="#969696", size=0.10, alpha=0.50, data = filter(percent_change, type == "weighted", date >= input$range[1], date <= input$range[2])) +
+      geom_line(show.legend = FALSE, data = states_dat) +
+      geom_dl(method=list("last.qp", fontfamily="Helvetica"), data = last_dp) +
       ylab("Percent change from average mortality") +
       xlab("Date") +
-      scale_y_continuous(label = scales::percent) +
-      scale_x_date(date_labels = "%b %Y") +
+      coord_cartesian(ylim = c(y_limits[1], y_limits[2])) +
+      scale_y_continuous(label = scales::percent, 
+                         breaks = seq(y_limits[1], y_limits[2], by=round((y_limits[2]-y_limits[1]) / 10, 5))) +
+      scale_x_date(date_labels = "%b %Y", 
+                   limits = c(input$range[1], input$range[2] + weeks(6))) +
       scale_color_manual(name = "",
                          values = my_palette) +
       scale_fill_manual(name = "",
                         values = my_palette) +
-      theme_slate() +
-      theme(legend.position = "top")
+      theme_slate()
+    
+    # ggplot() +
+    #   geom_hline(yintercept = 0, color="#525252", lty=2) +
+    #   geom_line(aes(date, fitted, group=state), color="#969696", size=0.10, alpha=0.50, data = filter(percent_change, type == "weighted", date >= input$range[1], date <= input$range[2])) +
+    #   geom_line(aes(date, fitted, color=state), show.legend = FALSE, data = states_dat) +
+    #   geom_dl(aes(date, fitted, label=state, color=state), method=list("smart.grid", fontfamily="Helvetica", cex=1.5), data = last_dp) +
+    #   ylab("Percent change from average mortality") +
+    #   xlab("Date") +
+    #   coord_cartesian(ylim = c(y_limits[1], y_limits[2])) +
+    #   scale_y_continuous(label = scales::percent, 
+    #                      breaks = seq(y_limits[1], y_limits[2], by=round((y_limits[2]-y_limits[1]) / 10, 5))) +
+    #   scale_x_date(date_labels = "%b %Y") +
+    #   scale_color_manual(name = "",
+    #                      values = my_palette) +
+    #   scale_fill_manual(name = "",
+    #                     values = my_palette) +
+    #   theme_slate()
   })
   
   output$percent_changeV2 <- renderPlot({
@@ -67,22 +99,62 @@ shinyServer(function(input, output) {
 })
 
 
-
-# temp_states = c("Massachusetts", "Florida", "Texas")
-# states_dat <- percent_change %>%
-#   filter(date >= "2020-05-01") %>%
-#   filter(type == "weighted") %>%
-#   filter(state %in% statsss) %>%
-#   mutate(state = paste0("  ", state))
 # 
+# temp_states <- c("Massachusetts", "Florida", "Texas", "Arkansas", "New York City")
+# states_dat  <- percent_change %>%
+#   filter(date >= "2020-01-01") %>%
+#   # filter(year(date) == 2019) %>%
+#   filter(type == "weighted") %>%
+#   filter(state %in% temp_states) %>%
+#   mutate(state = paste0("  ", state))
 # last_dp <- states_dat %>%
 #   group_by(state) %>%
 #   filter(date == max(date))
+# y_limits <- range(states_dat$fitted)
 # 
-# dl_names <- states_dat %>%
-#   group_by(state) %>%
-#   filter(date >= max(date)-8)
+# states_dat %>%
+#   ggplot(aes(date, fitted, label=state, color=state)) +
+#   geom_line(aes(date, fitted, group=state), color="#969696", size=0.10, alpha=0.50, data = filter(percent_change, date >= "2020-01-01", type == "weighted")) +
+#   geom_line(size=0.80, show.legend = FALSE, data = states_dat) +
+#   geom_dl(aes(date, fitted, label=state, color=state), method=list("smart.grid", fontface = "bold", fontfamily="Times"), data = last_dp) +
+#   ylab("Percent change from average mortality") +
+#   xlab("Date") +
+#   scale_y_continuous(label = scales::percent,
+#                      breaks = seq(y_limits[1], y_limits[2], by=round((y_limits[2]-y_limits[1]) / 10, 5))) +
+#   scale_x_date(date_labels = "%b %Y") +
+#   coord_cartesian(ylim = c(y_limits[1], y_limits[2])) +
+#   scale_color_manual(name = "",
+#                      values = my_palette) +
+#   scale_fill_manual(name = "",
+#                     values = my_palette) +
+#   theme_slate()
 # 
+# 
+# 
+# # dl_names <- states_dat %>%
+# #   group_by(state) %>%
+# #   filter(date >= max(date)-8)
+# ggplot() +
+#   geom_hline(yintercept = 0, color="#525252", lty=2) +
+#   # geom_point(aes(date, observed/expected - 1), alpha=0.50, show.legend = FALSE) +
+#   # geom_ribbon(aes(ymin=lwr, ymax=upr), alpha=0.20, color=NA, show.legend = FALSE) +
+#   # geom_point(aes(date, observed/expected - 1, color=state), alpha=0.50, show.legend = FALSE, data = states_dat) +
+#   # geom_ribbon(aes(date, ymin=lwr, ymax=upr, fill=state), alpha=0.20, color=NA, show.legend = FALSE, data = states_dat) +
+#   geom_line(aes(date, fitted, group=state), color="#969696", size=0.10, alpha=0.50, data = filter(percent_change, date >= "2020-01-01", type == "weighted")) +
+#   geom_line(aes(date, fitted, color=state), size=0.80, show.legend = FALSE, data = states_dat) +
+#   geom_dl(aes(date, fitted, label=state, color=state), method=list("smart.grid", fontface = "bold", fontfamily="Times"), data = last_dp) +
+#   ylab("Percent change from average mortality") +
+#   xlab("Date") +
+#   scale_y_continuous(label = scales::percent,
+#                      breaks = seq(y_limits[1], y_limits[2], by=round((y_limits[2]-y_limits[1]) / 10, 5))) +
+#   scale_x_date(date_labels = "%b %Y") +
+#   # coord_cartesian(ylim = c(-0.10, 1)) +round(y_limits[2] / 10, 2)
+#   coord_cartesian(ylim = c(y_limits[1], y_limits[2])) +
+#   scale_color_manual(name = "",
+#                      values = my_palette) +
+#   scale_fill_manual(name = "",
+#                     values = my_palette) +
+#   theme_slate()
 # 
 # ggplot() +
 #   geom_hline(yintercept = 0, color="#969696", lty=2, size=0.20) +
@@ -92,7 +164,7 @@ shinyServer(function(input, output) {
 #   geom_point(aes(date, fitted, group=state), color="#1C1E22", size=3, data = last_dp, show.legend = FALSE) +
 #   geom_point(aes(date, fitted, color=state), size=3, alpha=0.50, data = last_dp, show.legend = FALSE) +
 #   geom_point(aes(date, fitted, color=state), size=3, pch=1, data = last_dp, show.legend = FALSE) +
-#   geom_dl(aes(date, fitted, label=state, color=state), method=list("last.qp"), data = dl_names) +
+#   geom_dl(aes(date, fitted, label=state, color=state), method=list("smart.grid"), data = dl_names) +
 #   ylab("Percent change from average mortality") +
 #   xlab("Date") +
 #   scale_y_continuous(label = scales::percent) +
--- ui.r
+++ ui.r
@@ -2,7 +2,8 @@ source("init.R")
 load("rda/covid19-usa.rda")
 load("rda/cdc_counts.rda")
 states <- unique(percent_change$state)
-
+button_style <- "color: white; background-color: red; border-color: white; position: relative; 
+                     text-align:center; border-radius: 6px; border-width: 2px"
 
 
 
@@ -17,10 +18,29 @@ shinyUI(fluidPage(theme = shinytheme("slate"),
       tabPanel("Percent Change per States",
                
                # -- Tab Title
-               h2("Percent Change from Average Mortality", align = "center", style = "font-family: 'times'; color:white"),
-               p("We can add some text explaining the basics of the app just like FT does", 
-                 align = "center", style = "font-family: 'times'; font-size: 12pt ; color:white"),
+               h2("Percent Change from Average Mortality", align = "center", style = "font-family: 'helvetica'; color:white"),
                
+               # -- This should be an HTML tag to show when the app was last updated
+               p(em("Last updated on XX"), align = "center", style = "font-family: 'helvetica'; font-size: 8pt; color:#969696"),
+               
+               # -- Text to briefly explain inputs, data, and graphics
+               p("Here you can compare excess mortality trends for countries, US states, and cities around the world.", 
+                 align = "center", style = "font-family: 'helvetica'; font-size: 10pt ; color:#969696"),
+               
+               # -- Input: Countries, US states, or world cities
+               fluidRow(align = "center", 
+                        actionButton("c_countries", "Countries"),
+                        actionButton("c_states", "US States"),
+                        actionButton("c_cities", "World Cities")),
+               
+               # -- For spacing
+               br(),
+               
+               # -- Text to briefly explain inputs, data, and graphics
+               p("We can add some text explaining the basics of the app just like FT does", 
+                 align = "center", style = "font-family: 'helvetica'; font-size: 12pt ; color:white"),
+               
+               # -- Second set of inputs: Jurisdiction and Period. This should change depending on the input set 1
                fluidRow(
                  
                  # -- Jurisdiction input
@@ -41,11 +61,13 @@ shinyUI(fluidPage(theme = shinytheme("slate"),
                                        min    = min(cdc_counts$date),
                                        format = "M-dd-yyyy",
                                        max    = max(cdc_counts$date)))),
+
+               plotOutput("percent_change"))
+               # fluidRow(
+               #   
+               #   column(6, align = "center", plotOutput("percent_change")),
+               #   column(6, align = "center", plotOutput("percent_changeV2")))
                
-               fluidRow(
-                 
-                 column(6, align = "center", plotOutput("percent_change")),
-                 column(6, align = "center", plotOutput("percent_changeV2"))))
     ),
 ))
 
